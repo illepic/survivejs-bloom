52b8bce8e1b02022d32ce5dc6039de0e16945d52
Chris Bloom <bloomcb@gmail.com>
2015-07-20 07:50:07 -0700

    new react components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
-import Note from './Note';
+import Notes from './Notes';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes : [{
+        task: "Learn webpack"
+      }, {
+        task: "Learn React"
+      }, {
+        task: "Do Laundry"
+      }, {
+        task: "And another task"
+      }]
+    };
+  }
   render() {
-
-    var notes = [{
-      task: "Learn webpack"
-    }, {
-      task: "Learn React"
-    }, {
-      task: "Do Laundry"
-    }, {
-      task: "And another task"
-    }];
-
+    var notes = this.state.notes;
     return (
       <div>
-        <ul>{notes.map((note, i) =>
-          <li key={'note'+i}>
-            <Note value={note.task} />
-          </li>
-        )}</ul>
+        <button onClick={() => this.addItem()}>+</button>
+        <Notes items={notes} onEdit={(i, task) => this.itemEdited(i, task)} />
       </div>
     );
+  }
+  addItem() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        task: "New task"
+      }])
+    });
+  }
+  itemEdited(i, task) {
+    var notes = this.state.notes;
+    notes[i].taslk = task;
+    this.setState({
+      notes: notes
+    });
   }
 }
